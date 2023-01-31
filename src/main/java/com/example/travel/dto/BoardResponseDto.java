29c62f5d2a24e88306f1756ae9bf0f559977f6c2
jaeeoklim <lim080895@gmail.com>
2023-02-01 02:52:06 +0900

    46 - BoardWriter
    게시글 작성시 DB에 작성자의 닉네임이 저장 안 되는 오류 수정

--- src/main/java/com/example/travel/dto/BoardResponseDto.java
+++ src/main/java/com/example/travel/dto/BoardResponseDto.java
@@ -18,19 +18,15 @@ public class BoardResponseDto {
     private int hit; // 조회 수
     private LocalDateTime createdDate; // 생성일
     private LocalDateTime modifiedDate; // 수정일
-//    private String member_id; // 작성자 id
-//    private List<CommentResponseDto> comments;
 
     public BoardResponseDto(Board board) {
         this.id = board.getId();
         this.title = board.getTitle();
         this.content = board.getContent();
-        this.writer = board.getMember().getNickname();
+        this.writer = board.getWriter();
         this.thumb = board.getThumb();
         this.hit = board.getHit();
         this.createdDate = board.getCreatedDate();
         this.modifiedDate = board.getModifiedDate();
-//        this.member_id = board.getMember().getId();
-//        this.comments = board.getComments().stream().map(CommentResponseDto::new).collect(Collectors.toList());
     }
 }
