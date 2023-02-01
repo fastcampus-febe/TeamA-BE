8375f3a34ffacb9f807c9c8f4ad3c35311a2784c
ryusun <ryusunshine@gmail.com>
2023-02-02 01:48:23 +0900

    #51_마이페이지 & 비밀번호 변경
    1. 마이페이지내 회원정보 불러오기 기능
    2. 회원 비밀번호 변경 기능
    추가하였습니다.

--- src/main/java/com/example/travel/controller/MemberController.java
+++ src/main/java/com/example/travel/controller/MemberController.java
@@ -3,13 +3,12 @@ package com.example.travel.controller;
 import com.example.travel.dto.MemberLoginRequest;
 import com.example.travel.dto.MemberSignUpRequest;
 import com.example.travel.dto.TokenDto;
+import com.example.travel.entity.Member;
 import com.example.travel.service.MemberService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Api(tags = {"회원 서비스"}, description = "회원 서비스를 담당합니다.")
 @RequiredArgsConstructor
@@ -28,6 +27,28 @@ public class MemberController {
     public TokenDto signIn(@RequestBody MemberLoginRequest req){
         TokenDto res =  memberService.login(req);
         return res;
+    }
+
+    @ApiOperation(value = "마이페이지내 회원 정보", notes = "회원의 정보를 보여줍니다.")
+    @GetMapping("/myPage/info/{id}")
+    public Member mypageInfo(@PathVariable String id){
+        Member member = memberService.findById(id);
+        return member;
+    }
+
+    @ApiOperation(value = "패스워드 변경", notes = "패스워드를 확인후 변경합니다.")
+    @PostMapping("/myPage/changePassword/{id}")
+    public @ResponseBody String changePassword(@PathVariable("id") String id,
+                                               @RequestParam("currentPwd") String currentPwd,
+                                               @RequestParam("newPwd") String newPwd) {
+        if (currentPwd.equals(newPwd)){
+            return "비밀번호가 동일합니다.";
+        }
+        if (memberService.updatePwd(id, currentPwd, newPwd) == null){
+            return "비밀번호가 일치하지않습니다.";
+        } else {
+            return "비밀번호가 변경되었습니다.";
+        }
     }
 
 
