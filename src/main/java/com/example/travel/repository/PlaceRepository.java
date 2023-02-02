400bb16a350e3ce8ed3f64fa2a34f6f66c9f2c99
devSojoong <sojoong.dev@gmail.com>
2023-02-02 14:29:28 +0900

    #54- 관광지명으로 검색 시 결과 리스트 보여주기
    GET "/search/results?title=관광지명" 을 통해서 검색했을 때 place 의 title 컬럼과 비교하여 일치하는 데이터를 리스트로 보여준다.

--- src/main/java/com/example/travel/repository/PlaceRepository.java
+++ src/main/java/com/example/travel/repository/PlaceRepository.java
@@ -1,9 +1,11 @@
 package com.example.travel.repository;
 
 import com.example.travel.entity.Place;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.domain.Pageable;
 public interface PlaceRepository extends JpaRepository<Place, Integer> {
     Place findByContentId(int contentId);
+    Page<Place> findByTitleContaining(String title, Pageable pageable);
 }
 
