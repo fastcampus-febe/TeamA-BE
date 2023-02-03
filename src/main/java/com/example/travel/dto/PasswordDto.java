1616c9d0d2895c4772e8e6d75b32acca5a885213
ryusun <ryusunshine@gmail.com>
2023-02-03 20:30:23 +0900

    feat: 마이페이지&비밀번호 변경기능 추가

---
+++ src/main/java/com/example/travel/dto/PasswordDto.java
@@ -0,0 +1,15 @@
+package com.example.travel.dto;
+
+import lombok.*;
+
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PasswordDto {
+
+    private String currentPwd;
+    private String newPwd;
+
+}
